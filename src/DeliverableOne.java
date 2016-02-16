797cf2202c4dc0785089cd80b83fe103b0003ce8
djgonza1 <djgonza1@uci.edu>
2016-02-16 09:48:55 -0800

    First build.

---
+++ src/DeliverableOne.java
@@ -0,0 +1,14 @@
+
+
+
+public class DeliverableOne {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+	
+	public String CreateIndex(String filePath) {
+		
+		return "";
+	}
+}
